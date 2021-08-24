77f5f48468f31797a1a73b0fa49c91915a8d3894
tctproteam <67031797+tctproteam@users.noreply.github.com>
2021-08-24 15:46:37 -0700

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -4,7 +4,7 @@
 		return $.fn.countdown.begin(this, $.extend({
 			year: 2021, // YYYY Format
 			month: 8, // 1-12
-			day: 28, // 1-31
+			day: 27, // 1-31
 			hour: 23, // 24 hour format 0-23
 			minute: 59, // 0-59
 			second: 0, // 0-59
